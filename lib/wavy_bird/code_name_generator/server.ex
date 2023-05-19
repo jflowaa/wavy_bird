cb8608ff524e67c1dce74258b4f641ce0026d45e
Jack Flowers <jakaf1@gmail.com>
2023-05-19 18:13:10 -0500

    Move words list to ets

--- lib/wavy_bird/code_name_generator/server.ex
+++ lib/wavy_bird/code_name_generator/server.ex
@@ -13,21 +13,29 @@ defmodule WavyBird.CodeNameGenerator.Server do
   end
 
   def init(_) do
-    words =
-      File.stream!("data/words.txt")
-      |> Stream.map(&String.trim_trailing/1)
+    :ets.new(:words_table, [:set, :private, :named_table])
 
-    {:ok, words}
+    File.stream!("data/words.txt")
+    |> Stream.map(&String.trim_trailing/1)
+    |> Stream.with_index(1)
+    |> Stream.each(fn x ->
+      :ets.insert(:words_table, {elem(x, 1), elem(x, 0)})
+    end)
+    |> Stream.run()
+
+    {:ok, Keyword.get(:ets.info(:words_table), :size)}
   end
 
   def handle_call({:generate, number_of_words}, _from, state) do
     {:reply,
      {:ok,
-      Enum.map(1..1, fn _ ->
-        state
-        |> Enum.shuffle()
-        |> Enum.take(elem(Integer.parse(number_of_words), 0))
-        |> Enum.map(fn x -> Macro.camelize(x) end)
+      Enum.map(1..6, fn _ ->
+        Enum.map(1..elem(Integer.parse(number_of_words), 0), fn _ ->
+          :ets.lookup(:words_table, Enum.random(1..state))
+          |> hd
+          |> elem(1)
+          |> Macro.camelize()
+        end)
         |> Enum.join(" ")
       end)}, state}
   end
