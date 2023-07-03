1da8796c7d44915234c527c9457923faf9688eda
Jack Flowers <jakaf1@gmail.com>
2023-07-03 12:18:05 -0500

    update local deploy script

--- deployment/deploy_vm.sh
+++ deployment/deploy_vm.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-# echo yes | mix phx.gen.release --docker -- Need to copy over data/words.txt -- Is there a better way?
-cd ..
-tar -cvzf wavy_bird.tar.gz -X wavy_bird/deployment/exclude.txt wavy_bird
-scp wavy_bird.tar.gz root@digital-ocean:/root
+# echo yes | mix phx.gen.release --docker
+docker buildx build . -t registry.devtoolbelt.xyz/wavy-bird:latest --platform linux/amd64 --push && \
+scp deployment/docker-compose.yaml root@digital-ocean:/root/wavy_bird && \
+scp deployment/.env root@digital-ocean:/root/wavy_bird && \
+scp -r data/ root@digital-ocean:/root/wavy_bird/ && \
 ssh digital-ocean << EOF
-rm -rf wavy_bird/
-tar -xvf wavy_bird.tar.gz
-cd wavy_bird/
-docker build . -t wavy-bird --build-arg MIX_ENV="prod"
-docker-compose -f deployment/docker-compose.yaml -p wavy_bird down
-docker-compose -f deployment/docker-compose.yaml -p wavy_bird up -d
+docker-compose -f wavy_bird/docker-compose.yaml -p wavy_bird down && \
+docker-compose -f wavy_bird/docker-compose.yaml -p wavy_bird up -d
 EOF
-
-
-# echo yes | mix phx.gen.release --docker -- Need to copy over data/* -- Is there a better way?
-# docker buildx build . -t registry.devtoolbelt.xyz/wavy-bird:latest --build-arg MIX_ENV="prod" --platform linux/amd64 && \
-# docker push registry.devtoolbelt.xyz/wavy-bird:latest && \
-# scp deployment/docker-compose.yaml root@digital-ocean:/root/wavy_bird && \
-# ssh digital-ocean << EOF
-# docker-compose -f wavy_bird/docker-compose.yaml -p wavy_bird down && \
-# docker-compose -f wavy_bird/docker-compose.yaml -p wavy_bird up -d
-# EOF
